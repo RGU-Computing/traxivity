46183f8fe13d11ccc8fa0fed5f3e8017efc185d3
Janith Gamage <janithjayasankha@gmail.com>
2020-07-27 16:48:26 +0100

    folder re-structuring

---
+++ index.js
@@ -0,0 +1,5 @@
+import Today from './src/Today';
+import Weekly from './src/Weekly';
+import Monthly from './src/Monthly'
+
+export { Today, Weekly, Monthly };
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import Today from './Today';
-import Weekly from './Weekly';
-import Monthly from './Monthly'
-
-export { Today, Weekly, Monthly };
